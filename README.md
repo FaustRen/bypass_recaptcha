d70863b05f42ddcb2dddf1b05a11e2b92152671c
ShangRenHong <faustren1z@gmail.com>
2024-08-02 22:43:27 +0800

    docs: Add more introductions and explanations for this project.

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 
 bypass google recaptcha with python selenium without paying
 
+As demonstrated in the demo GIF below, the program will add two Chrome extensions in the Chrome options:
+
+Click the square box to the left of the "I'm not a robot" text
+Start bypassing the verification
+The bypass process will begin whenever you visit a webpage with Google verification.
 
 ## Installation
 ```bash
--- pass_recaptcha.py
+++ pass_recaptcha.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import selenium
 
 
+## example.1 Auto setting driver path
 url = "https://patrickhlauke.github.io/recaptcha/"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_extension("./crx_folder/auto_recaptcha_solver.crx")
 chrome_options.add_extension("./crx_folder/recaptcha_autoclick.crx")
 driver = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 driver.get(url)
+
+
+## example.2 Setting driver path by yourself
+# url = "https://patrickhlauke.github.io/recaptcha/"
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_extension("./crx_folder/auto_recaptcha_solver.crx")
+# chrome_options.add_extension("./crx_folder/recaptcha_autoclick.crx")
+# # driver_path = "please setting your driver path"
+# driver_path = "/Users/renren/Downloads/chromedriver-mac-arm64/chromedriver"
+# svc = Service(driver_path)
+# driver = webdriver.Chrome(service=svc,options=chrome_options)
+# driver.get(url)
