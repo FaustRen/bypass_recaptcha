ad78b871429402689e059804085087b41f90f9bf
ShangRenHong <faustren1z@gmail.com>
2024-08-04 17:09:57 +0800

    docs(modify demo gif):
    As title

--- pass_recaptcha.py
+++ pass_recaptcha.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import selenium
 
 
 ## example.1 Auto setting driver path
-url = "https://patrickhlauke.github.io/recaptcha/"
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_extension("./crx_folder/auto_recaptcha_solver.crx")
-chrome_options.add_extension("./crx_folder/recaptcha_autoclick.crx")
-driver = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-driver.get(url)
+# url = "https://testrecaptcha.github.io/"
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_extension("./crx_folder/auto_recaptcha_solver.crx")
+# chrome_options.add_extension("./crx_folder/recaptcha_autoclick.crx")
+# driver = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+# driver.get(url)
 
 
 ## example.2 Setting driver path by yourself
-# url = "https://patrickhlauke.github.io/recaptcha/"
+# url = "https://testrecaptcha.github.io/"
 # chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_extension("./crx_folder/auto_recaptcha_solver.crx")
 # chrome_options.add_extension("./crx_folder/recaptcha_autoclick.crx")
 # driver_path = "please setting your driver path"
-# svc = Service(driver_path)
+# svc = ChromeService(driver_path)
 # driver = webdriver.Chrome(service=svc,options=chrome_options)
 # driver.get(url)
